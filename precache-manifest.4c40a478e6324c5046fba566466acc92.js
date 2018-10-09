131c406b7ba619f99e7593225504cc1474ecc573
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-09 17:54:36 +0000

    Deploy felipecesr/front-end-challenge to github.com/felipecesr/front-end-challenge.git:gh-pages

---
+++ precache-manifest.4c40a478e6324c5046fba566466acc92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec4e9ec5bcebcf7946d3",
+    "url": "/front-end-challenge/static/js/runtime~main.ec4e9ec5.js"
+  },
+  {
+    "revision": "a9a46ff2b0027d4c6dd2",
+    "url": "/front-end-challenge/static/js/main.a9a46ff2.chunk.js"
+  },
+  {
+    "revision": "8bbf719b723561180e50",
+    "url": "/front-end-challenge/static/js/1.8bbf719b.chunk.js"
+  },
+  {
+    "revision": "a9a46ff2b0027d4c6dd2",
+    "url": "/front-end-challenge/static/css/main.64cda877.chunk.css"
+  },
+  {
+    "revision": "13285a981083b1a4a4c2fc4f56e524fa",
+    "url": "/front-end-challenge/index.html"
+  }
+];
